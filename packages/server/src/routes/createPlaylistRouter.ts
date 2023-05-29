52bde8ec57dec3d500e4758a3d25fd89a7e8e8d1
Casey <casey_turner@outlook.com>
2023-05-29 22:11:45 +1000

    update authenticate user middleware

---
+++ packages/server/src/routes/createPlaylistRouter.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from 'express'
+import { authenticateUser } from '../middlewares/authenticateUser.js'
+
+const router = express.Router()
+const jwtSecret = process.env.JWT_SECRET
+
+// Create playlist endpoint
+router.post(
+  '/create-playlist',
+  authenticateUser(jwtSecret),
+  (req: Request, res: Response) => {
+    console.log('req', req)
+    console.log('res', res)
+
+    // const { accessToken, userId } = req.userData
+    // const { tracks, playlistTitle } = req.body
+    // const playlistOptions = {
+    //   name: playlistTitle,
+    //   description: 'A playlist created with the Spotify API!',
+    //   public: false,
+    // }
+    // // Create a new playlist
+    // createPlaylist(accessToken, userId, playlistOptions)
+    //   .then((playlist) => {
+    //     const playlistId = playlist.id
+    //     const uris = getSongUris(tracks)
+    //     addTracksToPlaylist(accessToken, playlistId, uris)
+    //       .then((response) => {
+    //         logger(
+    //           logLevels.info,
+    //           'Playlist created successfully',
+    //           '/create-playlist',
+    //           response
+    //         )
+    //         res.status(200).json({ message: 'Playlist created successfully' })
+    //       })
+    //       .catch((error) => {
+    //         logger(logLevels.error, error.message, 'addTracksToPlaylist', error)
+    //         res.status(500).json({ message: 'Something went wrong' })
+    //       })
+    //   })
+    //   .catch((error) => {
+    //     logger(logLevels.error, error.message, '/create-playlist', error)
+    //     res.status(500).json({ message: 'Something went wrong' })
+    //   })
+  }
+)
+export default router
