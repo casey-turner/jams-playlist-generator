dd8a64f16699914f738f48b2f61be3640659c096
casey-turner <casey_turner@outlook.com>
2023-05-29 19:51:02 +1000

    update index.ts with new route

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,29 +3,24 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import Express from 'express'
 import { PORT } from './config'
+import generatePlaylistRouter from './routes/generatePlaylistRouter'
 import spotifyAuthRouter from './routes/spotifyAuthRouter'
 
 const corsOptions = {
   origin: 'http://localhost:5173',
+  credentials: true,
 }
 const app = Express()
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173') // Set the correct origin here
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS'
-  )
-  next()
-})
+
 app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(bodyParser.json())
-app.use('/', spotifyAuthRouter)
+app.use(spotifyAuthRouter)
+app.use(generatePlaylistRouter)
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(PORT as string, () => {
   console.log(`Server running http://localhost:${PORT}`)
