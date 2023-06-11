ce0e094fd9c9fee5da62277d5691d60ad928fc0f
casey-turner <casey_turner@outlook.com>
2023-06-11 17:29:25 +1000

    Refactor logger function to handle unknown data type in template literals

--- packages/server/src/utils/logger.ts
+++ packages/server/src/utils/logger.ts
@@ -10,27 +10,35 @@ const logger = (
   level: logLevels,
   message: string,
   src: string,
-  data: string
+  data: unknown
 ) => {
   switch (level) {
     case logLevels.info:
-      console.log(`[INFO] ${message}, src: ${src}, data: ${data}`)
-      break
+      console.log(`[INFO] ${message}, src: ${src}, data: ${formatData(data)}`);
+      break;
     case logLevels.warn:
-      console.warn(`[WARN] ${message}, src: ${src}, data: ${data}`)
-      break
+      console.warn(`[WARN] ${message}, src: ${src}, data: ${formatData(data)}`);
+      break;
     case logLevels.error:
-      console.error(`[ERROR] ${message}, src: ${src}, data: ${data}`)
-      break
+      console.error(`[ERROR] ${message}, src: ${src}, data: ${formatData(data)}`);
+      break;
     case logLevels.debug:
-      console.debug(`[DEBUG] ${message}, src: ${src}, data: ${data}`)
-      break
+      console.debug(`[DEBUG] ${message}, src: ${src}, data: ${formatData(data)}`);
+      break;
     case logLevels.trace:
-      console.log(`[TRACE] ${message}, src: ${src}, data: ${data}`)
-      break
+      console.log(`[TRACE] ${message}, src: ${src}, data: ${formatData(data)}`);
+      break;
     default:
-      console.log(`${message}, src: ${src}, data: ${data}`)
+      console.log(`${message}, src: ${src}, data: ${formatData(data)}`);
   }
-}
+};
+
+const formatData = (data: unknown): string => {
+  if (typeof data === 'string') {
+    return data;
+  }
+  return JSON.stringify(data);
+};
 
 export { logLevels, logger }
+
