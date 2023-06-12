2084bb7fb0de6779afea25be00afe2f581c72e83
casey-turner <casey_turner@outlook.com>
2023-06-12 21:30:59 +1000

    Add types for spotify api responses

--- packages/server/src/controllers/createPlaylistController.ts
+++ packages/server/src/controllers/createPlaylistController.ts
@@ -1,36 +1,60 @@
+import { AxiosResponse } from 'axios'
 import { spotifyApi } from '../apis/spotifyApi'
 import { PlaylistTrack } from '../types/playlistTypes'
+import {
+  AddTracksToPlaylistResponse,
+  CreatePlaylistResponse,
+} from '../types/spotifyTypes'
 import { logLevels, logger } from '../utils/logger'
 
-const createPlaylist = (accessToken: string, userId: string, options: { name: string; description: string; public: boolean }) => {
-  return spotifyApi
-    .post(`/users/${userId}/playlists`, options, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    })
-    .then((response) => response.data)
-    .catch((error) => {
-      logger(logLevels.error, 'error', 'createPlaylist', error)
-    })
+const createPlaylist = async (
+  accessToken: string,
+  userId: string,
+  options: { name: string; description: string; public: boolean }
+) => {
+  try {
+    const response: AxiosResponse<CreatePlaylistResponse> =
+      await spotifyApi.post(`/users/${userId}/playlists`, options, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+    return response.data
+  } catch (error) {
+    logger(
+      logLevels.error,
+      'Error occurred while creating the playlist',
+      'createPlaylist',
+      error
+    )
+  }
 }
 
 const getSongUris = (songs: PlaylistTrack[]) => {
   return songs.map((song) => song.uri)
 }
 
-const addTracksToPlaylist = async (accessToken: string, playlistId: string, uris: string[]) => {
-  return spotifyApi
-    .post(
-      `/playlists/${playlistId}/tracks`,
-      { uris },
-      {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      }
+const addTracksToPlaylist = async (
+  accessToken: string,
+  playlistId: string,
+  uris: string[]
+) => {
+  try {
+    const response: AxiosResponse<AddTracksToPlaylistResponse> =
+      await spotifyApi.post(
+        `/playlists/${playlistId}/tracks`,
+        { uris },
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      )
+    return response.data
+  } catch (error) {
+    logger(
+      logLevels.error,
+      'Error occurred while adding tracks to the playlist',
+      'addTracksToPlaylist',
+      error
     )
-    .then((response) => response.data)
-    .catch((error) => {
-      logger(logLevels.error, error.message, 'addTracksToPlaylist', error)
-    })
+  }
 }
 
 export { addTracksToPlaylist, createPlaylist, getSongUris }
-
