7bacc525b8cc3061e54bc8049344a9ba1ed6aef2
casey-turner <casey_turner@outlook.com>
2023-06-12 21:33:38 +1000

    Format playlistTypes.ts

--- packages/server/src/types/playlistTypes.ts
+++ packages/server/src/types/playlistTypes.ts
@@ -1,29 +1,29 @@
 export type PlaylistGenerationRequest = {
-  prompt: string; // Music genre supplied by the user
+  prompt: string // Music genre supplied by the user
   // TODO add other properties related to playlist generation
 }
 
-export type OpenAiResponse = { 
-  playlist?: Array<{ title: string, artist: string }>;
-  playlistTitles?: string[];
+export type OpenAiResponse = {
+  playlist?: Array<{ title: string; artist: string }>
+  playlistTitles?: string[]
 }
 
 export type SpotifyTrackData = {
-  name: string;
-  uri: string;
-  artists: { name: string }[];
-  album: { name: string; images: { url: string }[] };
+  name: string
+  uri: string
+  artists: { name: string }[]
+  album: { name: string; images: { url: string }[] }
 }
 
 export type PlaylistCreationRequest = {
-  tracks: PlaylistTrack[];
-  playlistTitle: string;
+  tracks: PlaylistTrack[]
+  playlistTitle: string
 }
 
 export type PlaylistTrack = {
-  title: string;
-  uri: string;
-  artist: string;
-  album: string;
-  albumCover: string;
+  title: string
+  uri: string
+  artist: string
+  album: string
+  albumCover: string
 }
