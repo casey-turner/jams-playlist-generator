3358df2b56b64642cbc7e80d302f2686dc7b2513
casey-turner <casey_turner@outlook.com>
2023-06-12 21:29:36 +1000

    add types for spotify api responses

--- packages/server/src/types/spotifyTypes.ts
+++ packages/server/src/types/spotifyTypes.ts
@@ -1,13 +1,22 @@
 export type SpotifyAuthData = {
-  accessToken: string,
-  refreshToken: string,
-  expiresIn: number,
-  timestamp: number,
-  userId: string,
+  accessToken: string
+  refreshToken: string
+  expiresIn: number
+  timestamp: number
+  userId: string
 }
 
 export type RefreshTokenResult = {
-  accessToken: string;
-  expiresIn: number;
-  error?: string;
+  accessToken: string
+  expiresIn: number
+  error?: string
+}
+
+export type CreatePlaylistResponse = {
+  id: string
+  href: string
+}
+
+export type AddTracksToPlaylistResponse = {
+  snapshot_id: string
 }
