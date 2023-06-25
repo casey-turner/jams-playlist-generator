c90f863876f76da6fe5dfaad8b518cf786440b88
casey-turner <casey_turner@outlook.com>
2023-06-25 19:38:38 +1000

    Add new routes

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,11 +1,26 @@
+import Connect from '@features/app/Connect'
+import CustomisePlaylist from '@features/app/CustomisePlaylist'
+import GeneratePlaylist from '@features/app/GeneratePlaylist'
+import Landing from '@features/app/Landing'
 import { useRoutes } from 'react-router-dom'
-import Landing from './features/app/Landing'
 
 function App() {
   let element = useRoutes([
     {
       path: '/',
       element: <Landing />,
+    },
+    {
+      path: 'connect',
+      element: <Connect />,
+    },
+    {
+      path: 'generate-playlist',
+      element: <GeneratePlaylist />,
+    },
+    {
+      path: 'customise-playlist',
+      element: <CustomisePlaylist />,
     },
   ])
   return element
