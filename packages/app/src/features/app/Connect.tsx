a76869f79fc18dc85e1ab75e087c49a15f528629
casey-turner <casey_turner@outlook.com>
2023-06-25 19:39:18 +1000

    Add connect to spotify button

--- packages/app/src/features/app/Connect.tsx
+++ packages/app/src/features/app/Connect.tsx
@@ -5,6 +5,7 @@ const Connect = () => {
     <>
       <Head title="Connect" description="Connect" />
       <div>Connect</div>
+      <a href="http://localhost:3000/connect">Connect to Spotify</a>
     </>
   )
 }
