5cfab66d5f1b7f63f60659d27c158fb5a9df14bb
casey-turner <casey_turner@outlook.com>
2023-07-03 19:20:55 +1000

    Added 404 page

---
+++ packages/app/src/features/pages/Error404.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '@components/Layout'
+
+const Error404 = () => {
+  return (
+    <>
+      <Layout>
+        <div>Error 404</div>
+      </Layout>
+    </>
+  )
+}
+
+export default Error404
