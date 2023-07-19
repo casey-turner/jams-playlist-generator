62b9cc29034f196421b94d1d9cba811c3b6e1212
casey-turner <casey_turner@outlook.com>
2023-07-19 21:01:05 +1000

    Added useLocalStorage hook to persist playlist data to local storage

---
+++ packages/app/src/features/playlist/hooks/useLocalStorage.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+const useLocalStorage = (key, defaultValue) => {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key)
+    return storedValue ? JSON.parse(storedValue) : defaultValue
+  })
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+
+  return [value, setValue]
+}
+
+export default useLocalStorage
