af8ac923457f6cd97b24693485f1109d5d486859
casey-turner <casey_turner@outlook.com>
2023-07-30 19:58:02 +1000

    Update landing page layout

--- packages/app/src/features/pages/Landing.tsx
+++ packages/app/src/features/pages/Landing.tsx
@@ -17,11 +17,11 @@ const Landing = () => {
   return (
     <>
       <Layout>
-        <div>
-          <h1 className="font-this text-6xl uppercase">
+        <div className="text-center lg:px-10  xl:px-40">
+          <h1 className="font-this text-alice-blue mb-5 text-6xl uppercase">
             Discover Your Perfect Playlist
           </h1>
-          <p>
+          <p className="text-alice-blue mb-10">
             Unleash the power of JAMS, where music meets AI technology! No more
             struggling to find the perfect playlist for your mood or favorite
             genre. With JAMS, simply input your desired music genre, and let AI
