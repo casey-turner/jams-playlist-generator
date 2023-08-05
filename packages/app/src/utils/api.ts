1b6a803fb968b5bf9650af2e85d9fcb9d54086c2
casey-turner <casey_turner@outlook.com>
2023-08-05 16:14:11 +1000

    [WIP] deploy

--- packages/app/src/utils/api.ts
+++ packages/app/src/utils/api.ts
@@ -11,21 +11,9 @@ const authClient = axios.create({
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Credentials': true,
+    Authorization: `Bearer ${token.get()}`,
     Accept: 'application/json',
   },
 })
-
-authClient.interceptors.request.use(
-  (config) => {
-    const authToken = token.get()
-    if (authToken) {
-      config.headers['Authorization'] = `Bearer ${authToken}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
 
 export default authClient
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         // target: 'http://localhost:3000',
         target: 'https://jams-playlist-generator-production.up.railway.app',
         changeOrigin: true,
-        secure: false,
+        secure: true,
         rewrite: (path) => path.replace('/api', ''),
       },
     },
