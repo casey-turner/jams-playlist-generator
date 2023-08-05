2553cc50cd935168ebd4c13652f13a85c7c09c32
casey-turner <casey_turner@outlook.com>
2023-08-05 17:09:52 +1000

    [WIP] deploy

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         // target: 'http://localhost:3000',
         target: 'https://jams-playlist-generator-production.up.railway.app',
         changeOrigin: true,
-        secure: true,
+        secure: false,
         rewrite: (path) => path.replace('/api', ''),
       },
     },
